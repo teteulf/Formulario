9c45d359f4b8b1a5ff678ecbdc1df000f92edb34
teteulf <theolefevre791@gmail.com>
2023-10-01 00:39:33 -0300

    css feito

---
+++ Simulaçao de login/funções.js
@@ -0,0 +1,26 @@
+function desapareceLogin(loginForm, cadastro) {
+  loginForm.classList.remove("hidden");
+  cadastro.classList.add("hidden");
+  loginForm.classList.add("formLogin");
+}
+
+const dadoSenha = document.querySelector(".senhaInput");
+
+function verificaSenha(event) {
+  const mensagem = document.querySelector("#senhaIncompativel");
+
+  if (event.target.value !== dadoSenha.value) {
+    mensagem.innerText = "As senhas precisam ser as mesmas";
+  } else {
+    mensagem.innerText = "";
+  }
+}
+function mostrarSenha(inputPassword) {
+  if (inputPassword.type === "password") {
+    inputPassword.type = "text";
+  } else {
+    inputPassword.type = "password";
+  }
+}
+
+export { desapareceLogin, verificaSenha, mostrarSenha };
--- Simulaçao de login/index.js
+++ Simulaçao de login/index.js
@@ -1,38 +1,54 @@
 import DadosCadastrais from "./Class.js";
+import { desapareceLogin, verificaSenha, mostrarSenha } from "./funções.js";
 
 const cadastro = document.querySelector("#formCadastro");
-
-const loginForm = document.querySelector("#formLogin");
-
+const loginForm = document.querySelector(".formLogin");
 const loginButton = document.querySelector(".login");
-
 const registroButton = document.querySelector(".cadastro");
-
 const buttonCadastro = document.querySelector(".botaoCadastro");
-
 const buttonEntrar = document.querySelector("#botaoLogin");
-
+const dadosInvalidos = document.querySelector("#dadosInvalidos");
 const arrayCadastros = [];
+const dadoNome = document.querySelector(".nomeInput");
+const dadoEmail = document.querySelector(".emailInput");
+const dadoDataDeNascimento = document.querySelector(".dataInput");
+const dadoSenha = document.querySelector(".senhaInput");
+const dadoConfirmarSenha = document.querySelector("#confirmarSenha");
+const imagemOlho = document.querySelector("#imagemOlho");
+const inputSenha = document.querySelector(".inputLoginSenha");
+
+dadoConfirmarSenha.addEventListener("input", (event) => {
+  verificaSenha(event);
+});
 
 loginButton.addEventListener("click", () => {
-  loginForm.classList.remove("hidden");
-  cadastro.classList.add("hidden");
+  desapareceLogin(loginForm, cadastro);
 });
 
 registroButton.addEventListener("click", () => {
   loginForm.classList.add("hidden");
   cadastro.classList.remove("hidden");
+  loginForm.classList.remove("formLogin");
+  cadastro.classList.add("formCadastro");
 });
 
 buttonCadastro.addEventListener("click", (event) => {
-  let dadoNome = document.querySelector(".nomeInput").value;
-  let dadoEmail = document.querySelector(".emailInput").value;
-  let dadoDataDeNascimento = document.querySelector(".dataInput").value;
-  let dadoSenha = document.querySelector(".senhaInput").value;
-
   arrayCadastros.push(
-    new DadosCadastrais(dadoNome, dadoEmail, dadoDataDeNascimento, dadoSenha)
+    new DadosCadastrais(
+      dadoNome.value,
+      dadoEmail.value,
+      dadoDataDeNascimento.value,
+      dadoSenha.value
+    )
   );
+
+  dadoNome.value = "";
+  dadoEmail.value = "";
+  dadoDataDeNascimento.value = "";
+  dadoSenha.value = "";
+  dadoConfirmarSenha.value = "";
+
+  desapareceLogin(loginForm, cadastro);
 
   console.log(arrayCadastros);
 });
@@ -40,16 +56,30 @@ buttonCadastro.addEventListener("click", (event) => {
 buttonEntrar.addEventListener("click", (event) => {
   let loginEmail = document.querySelector(".inputLoginEmail").value;
   let loginSenha = document.querySelector(".inputLoginSenha").value;
-  if (arrayCadastros) {
+
+  if (arrayCadastros.length === 0) {
+    dadosInvalidos.innerText = "*Dados inválidos*";
+  } else {
     arrayCadastros.forEach((elementoArray) => {
       if (
         elementoArray.email === loginEmail &&
         elementoArray.senha === loginSenha
       ) {
         console.log("voce entrou :D");
+        dadosInvalidos.innerText = "";
       } else {
-        ("acesso negado! D:");
+        dadosInvalidos.innerText = "Dados inválidos";
       }
     });
+  }
+});
+
+imagemOlho.addEventListener("click", function () {
+  if (imagemOlho.src.endsWith("olho-block.png")) {
+    imagemOlho.src = "assets/olho-vermelho.png";
+    mostrarSenha(inputSenha);
+  } else {
+    imagemOlho.src = "assets/olho-block.png";
+    mostrarSenha(inputSenha);
   }
 });
